5f09f94c08fc25731260da0c42700826ce789eb4
Mouhamed Youssef Diallo <mouhammed001@proton.me>
2026-08-25 23:39:11 +0000

    basic types (char , bool , unit)

--- src/main.rs
+++ src/main.rs
@@ -1,132 +1,83 @@
-
 #[allow(unused)]
-
-// // Remove something to make it work
+// Make it work
+// use std::mem::size_of_val;
 // fn main() {
-//     let x: u32 = 5;
-//     let mut y: u32 = 5;
+//     let c1 : char = 'a';// bytes
+//     assert_eq!(size_of_val(&c1),4); 
 
-//     y = x;
-    
-//     let z = 10; // Type of z ? 
+//     let c2 :char = '中'; // bytes
+//     assert_eq!(size_of_val(&c2),4); 
 
 //     println!("Success!");
+// } 
+
+// fn main()
+// {
+//     let chr : char = 'z';
+//      let st : &str = "Hello";
+//     println!("{} > {}" , st , chr);
 // }
 
+// // double quotion for string 
+// // single is for characters
 
-// Fill the blank
+
+// // Make it work
 // fn main() {
-//     let v: u16 = 38_u8 as u16;
+//     let c1 = '中';
+//     print_char(c1);
+// } 
 
-//     println!("Success!");
+// fn print_char(c : char) {
+//     println!("{}", c);
 // }
 
-// Modify `assert_eq!` to make it work
+// Make println! work
 // fn main() {
-//     let x = 5 ;
-//     assert_eq!("i32".to_string(), type_of(&x));
+//     let f: bool = true;
 
-//     println!("Success!");
-// }
-
-// // Get the type of given variable, return a string representation of the type  , e.g "i8", "u8", "i32", "u32"
-// fn type_of<T>(_: &T) -> String {
-//     format!("{}", std::any::type_name::<T>())
-// }
-
-
-// Fill the blanks to make it work
-// fn main() {
-//     assert_eq!(i8::MAX, 127); 
-//     assert_eq!(u8::MAX, 255); 
-
-//     println!("Success!");
-// }
-
-// Fix errors and panics to make it work
-// Fix errors and panics to make it work
-// fn main() {
-//    let v1 = 251_u32 + 8;
-//    let v2 = i32::checked_add(251, 8).unwrap();
-//    println!("{},{}",v1,v2);
-// }
-
-// fn main() {
-//     let v = 1_024 + 0xff + 0o77 + 0b1111_1111;
-//     println!("{}" , v);
-//     assert!(v == 1597);
-
-//     println!("Success!");
-// }
-
-
-// fn main() {
-//     let x = 1_000.000_1 as f32; // f32
-//     let y: f32 = 0.12; // f32
-//     let z = 0.01_f64; // f64
-
-//     assert_eq!(type_of(&x), "f32".to_string());
-//     println!("Success!");
-// }
-
-// fn type_of<T>(_: &T) -> String {
-//     format!("{}", std::any::type_name::<T>())
-// }
-
-// fn main() {
-//     let num = 0.1+0.2 as f32;
-//     assert!(num ==0.3);
-
-//     println!("Success!");
-// }
-
-// fn main() {
-//     let mut sum = 0;
-//     for i in -3..2 {
-//         sum += i
+//     let t : bool = true;
+//     if !!t {
+//         println!("Success!");
 //     }
+// } 
 
-//     assert!(sum == -5);
 
-//     for c in 'a'..='z' {
-//         println!("{}",c);
-//         println!("{}",c as u32);
-//     }
-// }
-
-// use std::ops::{Range, RangeInclusive};
+// // Make it work
 // fn main() {
-//     assert_eq!((1..5), Range{ start: 1, end: 5 });
-//     assert_eq!((1..10), RangeInclusive::new(1, 10));
-//     // println!("{}",  1..5);
+//     let f = true;
+//     let t : bool = false && true;
+//    assert_ne!(!f , t, "we are testing that the values are not equal");
+//     assert_eq!(t, f);
+//     println!("{}"  , t);
 
 //     println!("Success!");
 // }
 
+// // Make it work, don't modify `implicitly_ret_unit` !
+// fn main() {
+//     let _v: () = ();
 
-// Fill the blanks and fix the errors
-fn main() {
-    // Integer addition
-    assert!(1u32 + 2 == 3);
+//     let v  = ();
+//     assert_eq!(v, implicitly_ret_unit());
 
-    // Integer subtraction
-    assert!(1i32 - 2 == -1);
-    assert!(2u8 - 1 == 1); 
-    
-    assert!(3 * 50 == 150);
+//     println!("Success!");
+// }
 
-    assert!(9.6 / 3.2 == 4.0); // error ! make it work
+// fn implicitly_ret_unit() {
+//     println!("I will return a ()");
+// }
 
-    // assert!(24 % 5 == __);
-    // // Short-circuiting boolean logic
-    // assert!(true && false == __);
-    // assert!(true || false == __);
-    // assert!(!true == __);
+// // Don't use this one
+// fn explicitly_ret_unit() -> () {
+//     println!("I will return a ()");
+// }
 
-    // Bitwise operations
-    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
-    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
-    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
-    println!("1 << 5 is {}", 1u32 << 5);
-    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
-}
+// Modify `4` in assert to make it work
+// use std::mem::size_of_val;
+// fn main() {
+//     let unit: () = ();
+//     assert!(size_of_val(&unit) == 0);
+
+//     println!("Success!");
+// }
