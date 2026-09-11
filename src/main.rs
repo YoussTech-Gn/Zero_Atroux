e094c0d3e8c881d750edc5eb4ec63555b183235d
Mouhamed Youssef Diallo <mouhammed001@proton.me>
2026-09-11 19:53:53 +0000

    Struct & Method & self &self & imp

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,98 @@
-// // use String::from;
+// #[derive(Debug)]
+#[allow(unused)]
+// #[allow(unused_variables)]
 
-// struct User {
-//     name : String,
-//     email : String,
-//     active: bool
-// }
-// struct Color(i32 , i32 , u8);
-// struct Point(i32 , i32 , ());
-// fn main(){
-//     let mut new = build_new_user(String::from("h@proton.me"), String::from("Saliou"));
-//     println!("we have new his called [{}] his email Adress : [{}] and his connected right now! {}" , new.name , new.email , new.active );
-//     println!("we have new his called [{}] his email Adress : [{}] and his connected right now! {}" , new.name , new.email , new.active );
-//     let  mut same_user = &mut new;
-//     same_user.email = String::from("Mouhammed@pront.me");
-//     println!("we have new his called [{}] his email Adress : [{}] and his connected right now! {}" , new.name , new.email , new.active );
-    
-
-//     // ----------
-//     let black = Color(21 , 5, 0);
-//     let origin : Point = Point(8 , 10,  ());
+struct Products{
+    id : u32,
+    name : String,
+    desc : String,
+    img : String,
+    price : u32
+}
+// #[derive(Debug)]
+// struct  Currency {
+//     dollar : u32,
+//     cfa : u32,
 // }
 
-// fn build_new_user(email : String , name : String) -> User{
-//     User {
-//         active : true,
-//         email,
-//         name
+// impl Products {
+//     // fn total_price(&self,  count : u32) -> u32{
+//     //     self.price * count
+//     // }
+//     // fn total_price_in_cfa(dollar : u32) -> u32 {
+//     //     dollar * 500
+//     // }
+
+//     fn total_price(&self ,count : u32) -> Currency{
+//         Currency { dollar: self.price * count , cfa: (self.price * count) * 500  }
+//     }
+//     fn add_discount(&mut self , discount : u32){
+//         self.price -= discount;
 //     }
 // }
 
-#[derive(Debug)]
-
-struct  Reactangle {
-    width : i32,
-    height : i32,
-}
-
-fn main(){
-    // let w = 23;
-    // let h = 23;
-    // let rect : (i32 , i32)= (40  , 20);
-    let rect : Reactangle = Reactangle { width: 20, height: 30 };
-    // let x = rect;
-    println!("Our Struct is {:#?}" , rect);
-    println!("The area of the rectangle is {} square pixels." , area(&rect));
-    eprintln!("Erro Gys");
-    // println("jf");
-    println!();
-    println!();
-    dbg!(&rect);
-}
-
+// fn main(){
+//     let mut x = Products {
+//         id : 1,
+//         desc :String::from("Some Describtion for this Product"),
+//         img : String::from("Kalou"),
+//         name : String::from("Kardou"),
+//         price : 300
+//     };
+//     println!("our new Product : {:#?} " , x);
+//     let Products {id , ref name  , ref desc ,ref  img , price} = x;
+//     // println!("our new Product : {:#?} " , {name , desc , id , img})
+//     println!("name : {} , id : {} , desc : {} , img : {}" , name  ,id , x.desc , x.img);
+//     println!("our new Product : {:#?} " , x);
+//     let total : Currency = x.total_price(10);
+//     // println!("the total : [{}]$  | cfa [{}] " , total , Products::total_price_in_cfa(total));
+//     println!("the total : [{}]$  | cfa [{}] " , total.dollar , total.cfa);
+//     println!("function return a struct : {:#?} " , return_struct());
+//     x.add_discount(1000);
+//     println!("product after discount : {:#?} " , x );
+//     // println!("product after discount : {:#?} " , x.add_discount(100));
 
 
-fn area(dimension : &Reactangle) -> i32 {
-    dimension.width * dimension.height
-}
+// }
+
+
+// fn return_struct()-> Currency{
+//     Currency { dollar: 100, cfa: 60000 }
+// }
+
+
+ fn main() {
+     let mut text = String::from("Rust");
+          let r1 = &text;
+         let r2 = &text;
+         println!("Read: {}, {} ", r1, r2);
+  let r3 = &mut text;
+    println!("Read and Write: {}",  r3);
+ }
+
+// struct Player {
+//     name: String,
+//     score: u32,
+// }
+
+// impl Player {
+//     fn display_score(&self) {
+//         println!("Score: {}", self.score);
+//     }
+// }
+
+// fn main() {
+//     let p = Player { name: String::from("Youssouf"), score: 100 };
+//     let Player { ref name, ref  score } = p;
+//     p.display_score();
+// }
+
+// fn main(){
+// let mut x = Products { id: 1, price: 300 /*...*/ };
+// let my_borrow = &mut x;
+
+// my_borrow.add_discount(50); // هنا تكمن المشكلة
+
+// println!("{:?}", my_borrow.price);
+
+// }
